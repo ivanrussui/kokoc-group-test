a1967768b3654b5e9b55e3fb2283f57df0706068
ivanrussui <fenomen_ny@mail.ru>
2023-09-23 16:35:11 +0300

    fix: изображение и текст

--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -6,6 +6,7 @@ import styles from './ListItem.module.scss';
 
 export const ListItem: React.FC<{ data: ItemType }> = ({ data }) => {
     const [like, setLike] = useState(data.voteStatus);
+    const [isHidden, setIsHidden] = useState(true);
 
     const toggleImage = () => {
         setLike(!like);
@@ -17,13 +18,19 @@ export const ListItem: React.FC<{ data: ItemType }> = ({ data }) => {
         }
     };
 
+    const toggleActive = () => {
+        setIsHidden(!isHidden);
+    };
+
+    const textClassName = `${styles.Description__Text} ${isHidden ? styles.Description__Text_Hidden : ''}`;
+
     return (
         <li className={styles.ListItem}>
             <img src={data.imgUrl} alt="img" className={styles.ListItem__Img}/>
             <div className={styles.Description}>
                 <h2 className={styles.Description__Name}>{data.name}</h2>
                 <span className={styles.Description__Date}>{data.date}</span>
-                <p className={styles.Description__Text}>{data.text}</p>
+                <p className={textClassName} onClick={toggleActive}>{data.text}</p>
                 <div className={styles.Description__Bottom}>
                     <h3 className={styles.Description__Author}>{data.author}</h3>
                     <div className={styles.Description__Like} onClick={toggleImage}>
