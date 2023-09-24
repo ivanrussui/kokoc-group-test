00f5023913cc15ef67e805cdd3375383cb89e192
ivanrussui <fenomen_ny@mail.ru>
2023-09-24 03:36:32 +0300

    feat: animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {List} from "./components/List/List";
 import styles from './App.module.scss';
 
 function App() {
+    const [isShow, setShow] = useState(true);
+
+    useEffect(() => {
+        const animationTimeout = setTimeout(() => {
+            setShow(true);
+        }, 20);
+        return () => {
+            clearTimeout(animationTimeout);
+        };
+    }, []);
+
+    const titleClassName = `${styles.Title} ${isShow && styles.Title_Animation}`;
 
     return (
         <div className={styles.App}>
             <div className={styles.Container}>
-                <h1 className={styles.Title}>Новости</h1>
+                <h1 className={titleClassName}>Новости</h1>
                 <List/>
             </div>
         </div>
--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {LikeEmpty} from "./Like/LikeEmpty";
 import {LikeFill} from "./Like/LikeFill";
 import {ItemType} from "./ItemType";
@@ -7,6 +7,16 @@ import styles from './ListItem.module.scss';
 export const ListItem: React.FC<{ data: ItemType }> = ({ data }) => {
     const [like, setLike] = useState(data.voteStatus);
     const [isHidden, setIsHidden] = useState(true);
+    const [isShow, setShow] = useState(true);
+
+    useEffect(() => {
+        const animationTimeout = setTimeout(() => {
+            setShow(true);
+    }, 20);
+        return () => {
+            clearTimeout(animationTimeout);
+        };
+    }, []);
 
     const toggleImage = () => {
         setLike(!like);
@@ -22,11 +32,12 @@ export const ListItem: React.FC<{ data: ItemType }> = ({ data }) => {
         setIsHidden(!isHidden);
     };
 
-    const textClassName = `${styles.Description__Text} ${isHidden ? styles.Description__Text_Hidden : ''}`;
+    const textClassName = `${styles.Description__Text} ${isHidden && styles.Description__Text_Hidden}`;
+    const itemClassName = `${styles.ListItem} ${isShow && styles.ListItem_NewsAnimation}`;
 
     return (
-        <li className={styles.ListItem}>
-            <img src={data.imgUrl} alt="img" className={styles.ListItem__Img}/>
+        <li className={itemClassName}>
+            <img src={data.imgUrl} alt={data.name} className={styles.ListItem__Img}/>
             <div className={styles.Description}>
                 <h2 className={styles.Description__Name}>{data.name}</h2>
                 <span className={styles.Description__Date}>{data.date}</span>
